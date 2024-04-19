25db52379b98d8806c507581d551be110f07a69d
chamny20 <dge3179@ajou.ac.kr>
2024-04-19 14:21:11 +0900

    test: object test

--- src/test/sample.test.ts
+++ src/test/sample.test.ts
@@ -7,9 +7,17 @@ it("add corretly", () => {
 test("test minus calculator", () => {
   expect(minus({ a: 3, b: 5 })).toBe(-2);
 });
+
 // it("ENV Test", async () => {
 //   const testApi = process.env.REACT_APP_TEST_API;
 //   if (testApi) {
 //     expect(testApi).toBe("https://jsonplaceholder.typicode.com/posts/1");
 //   }
 // });
+
+// object의 값을 체크하고 싶을 경우에는 toEqual() 사용
+test("object assignment", () => {
+  const data: any = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
