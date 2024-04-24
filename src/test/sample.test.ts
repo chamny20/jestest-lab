7b09812cea09a595a1589f2b09563ec623bea8d4
chamny20 <dge3179@ajou.ac.kr>
2024-04-24 14:34:49 +0900

    test: test regular-expression and array

--- src/test/sample.test.ts
+++ src/test/sample.test.ts
@@ -40,3 +40,32 @@ test("zero", () => {
   expect(z).not.toBeTruthy();
   expect(z).toBeFalsy();
 });
+
+test("adding floating point numbers", () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           This won't work because of rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+
+// 정규표현식
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect("Christoph").toMatch(/stop/);
+});
+
+const shoppingList = [
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "milk",
+];
+
+// Array test
+test("the shopping list has milk on it", () => {
+  expect(shoppingList).toContain("milk");
+  expect(new Set(shoppingList)).toContain("milk");
+});
