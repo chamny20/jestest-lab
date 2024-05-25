4ced3411dee9384e2425a9234bdbcbfc6afa62a1
chamny20 <chamny20@naver.com>
2024-05-25 16:33:01 +0900

    chore: comment 처리

--- src/test/asynchronous.test.ts
+++ src/test/asynchronous.test.ts
@@ -1,30 +1,30 @@
-import { fetchData } from "utils/asynchoronous";
+// import { fetchData } from "utils/asynchoronous";
 
-test("the data is naver data", () => {
-  return fetchData().then((data) => {
-    expect(data).toBe("naver data");
-  });
-});
+// test("the data is naver data", () => {
+//   return fetchData().then((data) => {
+//     expect(data).toBe("naver data");
+//   });
+// });
 
-test("the data is naver data", async () => {
-  const data = await fetchData();
-  expect(data).toBe("naver data");
-});
+// test("the data is naver data", async () => {
+//   const data = await fetchData();
+//   expect(data).toBe("naver data");
+// });
 
-test("the fetch fails with an error", async () => {
-  // 테스트가 실행되면서 1개의 어설션을 실행해야 한다.
-  expect.assertions(1);
-  try {
-    await fetchData();
-  } catch (error) {
-    expect(error).toMatch("error");
-  }
-});
+// test("the fetch fails with an error", async () => {
+//   // 테스트가 실행되면서 1개의 어설션을 실행해야 한다.
+//   expect.assertions(1);
+//   try {
+//     await fetchData();
+//   } catch (error) {
+//     expect(error).toMatch("error");
+//   }
+// });
 
-test("the data is naver", async () => {
-  await expect(fetchData()).resolves.toBe("naver data");
-});
+// test("the data is naver", async () => {
+//   await expect(fetchData()).resolves.toBe("naver data");
+// });
 
-test("the fetch fails with an error", async () => {
-  await expect(fetchData()).rejects.toMatch("error");
-});
+// test("the fetch fails with an error", async () => {
+//   await expect(fetchData()).rejects.toMatch("error");
+// });
--- src/utils/asynchoronous.tsx
+++ src/utils/asynchoronous.tsx
@@ -1,7 +1,7 @@
-export async function fetchData() {
-  const response = await fetch("https://www.naver.com", {
-    method: "GET",
-  });
-  const data = await response.json();
-  console.log(data);
-}
+// export async function fetchData() {
+//   const response = await fetch("https://www.naver.com", {
+//     method: "GET",
+//   });
+//   const data = await response.json();
+//   console.log(data);
+// }
