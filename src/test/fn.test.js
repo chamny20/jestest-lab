45176f4cf2f619f41e57ed9b658d7ddf6f0eb641
chamny20 <dge3179@ajou.ac.kr>
2024-10-01 22:22:27 +0900

    toStrictEqual Matchers 개념 test #2

--- src/test/fn.test.js
+++ src/test/fn.test.js
@@ -1,4 +1,4 @@
-import { add } from "utils/fn";
+import { add, makeUser } from "utils/fn";
 
 test("1은 1이야.", () => {
     expect(1).toBe(1);
@@ -11,3 +11,26 @@ test('2 더하기 3은 5야.', () => {
 test('3 더하기 3은 5가 아니야.', () => {
     expect(add(3, 3)).not.toBe(5);
 })
+
+// test('이름과 나이를 전달받아 객체 반환해줘', () => {
+//     expect(makeUser("Chaemin", 23)).toBe({
+//         name: "Chaemin",
+//         age: 23,
+//     });
+// });
+
+// => 객체는 toEuql 사용
+
+test('이름과 나이를 전달받아 객체 반환해줘', () => {
+    expect(makeUser("Chaemin", 23)).toEqual({
+        name: "Chaemin",
+        age: 23,
+    });
+});
+
+test('이름과 나이를 전달받아 객체 반환해줘', () => {
+    expect(makeUser("Chaemin", 23)).toStrictEqual({
+        name: "Chaemin",
+        age: 23,
+    });
+});
