0ac1ae05a9b9006865a44593037f2679aca2362e
chamny20 <dge3179@ajou.ac.kr>
2024-10-01 22:41:09 +0900

    num compare 관련 테스트 #2

--- src/test/fn.test.js
+++ src/test/fn.test.js
@@ -59,3 +59,31 @@ test('0은 false이다.', () => {
 test('비어있지 않은 문자열은 true이다.', () => {
     expect(add('hello', 'world')).toBeTruthy();
 });
+
+// toBeGreaterThan 크다
+// toBeGreaterThanOrEqual 크거나 같다
+// toBeLessThan 작다
+// toBeLessThanOrEqual 작거나 같다
+
+test('id는 10자 이하여야 한다.', () => {
+    const id = 'THE_BLACK';
+    expect(id.length).toBeLessThanOrEqual(10);
+});
+
+test('비밀번호는 정확히 4자리여야 한다.', () => {
+    const pw = '1234';
+    expect(pw.length).toBe(4);
+});
+
+test('비밀번호는 정확히 4자리여야 한다.', () => {
+    const pw = '1234';
+    expect(pw.length).toEqual(4);
+});
+
+// 주의
+test('0.1 더하기 0.2는 0.3이다.', () => {
+    // expect(add(0.1, 0.2)).toBe(0.3);
+    // 위처럼 사용하면, js에선 소수를 이진법으로 바꾸었기 때문에 조금씩 달라지는 문제점 발생
+    expect(add(0.1, 0.2)).toBeCloseTo(0.3);
+    // 위처럼 toBeCloseTo 사용
+});
